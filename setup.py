ca5b43aa3d2d6aedb551d93c767f8ceaf17572c6
Orellabac <orellabac@gmail.com>
2022-11-18 03:41:23 +0000

    including readme in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 VERSION = '0.0.1'
 
 setup(name='snowpark_extensions',
       version=VERSION,
       description='A set of helpers to extend snowpark functionality',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/MobilizeNet/snowpark-extensions-py',
       author='mauricio.rojas',
       requires=['snowflake-snowpark-python','pandas','shortuuid'],
