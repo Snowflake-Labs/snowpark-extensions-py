c50c7db3b854d7299053866a7166152ca6f7d32c
Orellabac <orellabac@gmail.com>
2022-12-17 16:43:27 +0000

    adding test for explode_outer

--- tests/test_dataframe_extensions.py
+++ tests/test_dataframe_extensions.py
@@ -108,3 +108,36 @@ def test_explode_with_array():
     assert len(results) == 2
     assert results[0].ID == 1 and results[0].COL == '"foo"'
     assert results[1].ID == 1 and results[1].COL == '"bar"'
+
+
+def test_explode_outer_with_array():
+    from snowflake.snowpark import Session
+    import snowpark_extensions
+    from snowflake.snowpark.functions import explode_outer
+    session = Session.builder.appName('snowpark_extensions_unittest').from_snowsql().getOrCreate()
+    schema = StructType([StructField("id", IntegerType()), StructField("an_array", ArrayType()), StructField("a_map", MapType()) ])
+    sf_df = session.createDataFrame(
+        [(1, ["foo", "bar"], {"x": 1.0}), (2, [], {}), (3, None, None)],
+        schema)
+    #  +---+----------+----------+                                                     
+    # | id|  an_array|     a_map|
+    # +---+----------+----------+
+    # |  1|[foo, bar]|{x -> 1.0}|
+    # |  2|        []|        {}|
+    # |  3|      null|      null|
+    # +---+----------+----------+
+
+    results = sf_df.select("id", "an_array", explode_outer("an_array")).collect()
+    # +---+----------+----+
+    # | id|  an_array| col|
+    # +---+----------+----+
+    # |  1|[foo, bar]| foo|
+    # |  1|[foo, bar]| bar|
+    # |  2|        []|null|
+    # |  3|      null|null|
+    # +---+----------+----+
+    assert len(results) == 4
+    assert results[0].ID == 1 and results[0].COL == '"foo"'
+    assert results[1].ID == 1 and results[1].COL == '"bar"'
+    assert results[2].ID == 2 and results[2].COL == None
+    assert results[3].ID == 3 and results[3].COL == None
