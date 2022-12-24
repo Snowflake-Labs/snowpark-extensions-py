420765e4c4e9ce498c994e4d2671650a9d65eb92
Orellabac <orellabac@gmail.com>
2022-12-24 01:32:42 +0000

    missing version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.12'
+VERSION = '0.0.13'
 
 setup(name='snowpark_extensions',
       version=VERSION,
