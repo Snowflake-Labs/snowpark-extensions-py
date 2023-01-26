46736c0694a0080d1998d4e63a70a63e687cfbc2
Orellabac <orellabac@gmail.com>
2023-01-26 16:54:34 -0600

    0.0.19 (#20)
    * fixing issue with bround
    
    * adjustment for displayHTML
    
    * typo
    
    * Update setup.py
    
    * adjust for datediff
    
    * 0.0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.17'
+VERSION = '0.0.19'
 
 setup(name='snowpark_extensions',
       version=VERSION,
