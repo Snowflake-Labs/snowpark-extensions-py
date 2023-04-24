1382193b56453d11189f9dfe26db832cf73f0189
Jose Venegas <126916083+sfc-gh-jvaenegasvega@users.noreply.github.com>
2023-04-24 09:08:41 -0600

    Updating data frame reader extensions to support csv function

--- tests/test_dataframe_reader_extensions.py
+++ tests/test_dataframe_reader_extensions.py
@@ -1,10 +1,26 @@
 import pytest
-from snowflake.snowpark import Session, Row
+from snowflake.snowpark import Session, Row, DataFrameReader
 from snowflake.snowpark.types import *
 import snowpark_extensions
 
 def test_load():    
     session = Session.builder.from_snowsql().getOrCreate()
+    cases = session.read.load(["./tests/data/test1_0.csv","./tests/data/test1_1.csv"],
+                        schema=get_schema(),
+                        format="csv", 
+                        sep=",",
+                        header="true")
+    assert 10 == len(cases.collect())
+
+def test_csv():
+    session = Session.builder.from_snowsql().getOrCreate()
+    csvInfo = session.read.csv(["./tests/data/test1_0.csv","./tests/data/test1_1.csv"],
+                               schema=get_schema(),
+                               sep=",",
+                               header="true")
+    assert 10 == len(csvInfo.collect())
+
+def get_schema():
     schema = StructType([ \
     StructField("case_id",       StringType()), \
     StructField("province",      StringType()), \
@@ -13,11 +29,6 @@ def test_load():
     StructField("infection_case",StringType()), \
     StructField("confirmed",     IntegerType()), \
     StructField("latitude",      FloatType()), \
-    StructField("cilongitudety", FloatType()) \
+    StructField("longitude", FloatType()) \
     ])
-    cases = session.read.load(["./tests/data/test1_0.csv","./tests/data/test1_1.csv"],
-                        schema=schema,
-                        format="csv", 
-                        sep=",",
-                        header="true")
-    assert 10 == len(cases.collect())
+    return schema
